f2daca0235197f231931d0f798c14e719fec2f02
HosseinAbianeh <hosseinabianeh@yahoo.com>
2022-02-23 10:48:12 -0500

    good

--- testing camera.py
+++ testing camera.py
@@ -1,16 +1,26 @@
 import cv2
+import numpy
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 while True:
     ret, img = cap.read()
+    #print(type(img)) #This is a <class 'numpy.ndarray'>
+    print(img.shape) # img is a numpy matrix 480 x 640 x 3
+
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny = cv2.Canny(blur, 10, 70)
+    blur = cv2.GaussianBlur(img, (7, 7), 0)
+    canny = cv2.Canny(blur, 20, 70)
     ret, mask = cv2.threshold(canny, 70, 255, cv2.THRESH_BINARY)
     cv2.imshow('Video feed', mask)
 
+
+
+
+
     if cv2.waitKey(1) == 13:
         break
+
+
 cap.release()
 cv2.destroyAllWindows()
