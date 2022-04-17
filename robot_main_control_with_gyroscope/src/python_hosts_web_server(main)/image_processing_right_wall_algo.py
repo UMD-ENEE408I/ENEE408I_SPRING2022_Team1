9315db4a0ed33034ef9817ee8fbb095c150c9479
HosseinAbianeh <habianeh@umd.edu>
2022-04-17 04:36:18 -0400

    grdg

--- robot_main_control_with_gyroscope/src/python_hosts_web_server(main)/image_processing_right_wall_algo.py
+++ robot_main_control_with_gyroscope/src/python_hosts_web_server(main)/image_processing_right_wall_algo.py
@@ -20,7 +20,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((SERVER_HOST, SERVER_PORT))
 s.listen(0)
-s.settimeout(3)
+s.settimeout(2)
 
 SERVER_HOST = '192.168.0.14'
 SERVER_PORT2 = 8001
@@ -28,7 +28,7 @@ s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s2.bind((SERVER_HOST, SERVER_PORT2))
 s2.listen(0)
-s2.settimeout(3)
+s2.settimeout(2)
 
 SERVER_HOST = '192.168.0.14'
 SERVER_PORT3 = 8002
@@ -36,7 +36,7 @@ s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s3.bind((SERVER_HOST, SERVER_PORT3))
 s3.listen(0)
-s3.settimeout(3)
+s3.settimeout(2)
 
 
 
